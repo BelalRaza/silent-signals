f720484161a0b419319ca1de66aa650a761c062c
BelalRaza <br49009@gmail.com>
2024-09-22 23:42:38 +0530

    pushed

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -4,20 +4,29 @@ import mongoose from 'mongoose';
 import { User } from 'next-auth';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../auth/[...nextauth]/options';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
-  //request: Request
   await dbConnect();
   const session = await getServerSession(authOptions);
   const _user: User = session?.user;
 
   if (!session || !_user) {
-    return Response.json(
+    return NextResponse.json(
       { success: false, message: 'Not authenticated' },
       { status: 401 }
     );
   }
+
+  if (!_user._id) {
+    return NextResponse.json(
+      { success: false, message: 'User ID is missing' },
+      { status: 400 }
+    );
+  }
+
   const userId = new mongoose.Types.ObjectId(_user._id);
+
   try {
     const user = await UserModel.aggregate([
       { $match: { _id: userId } },
@@ -27,21 +36,19 @@ export async function GET() {
     ]).exec();
 
     if (!user || user.length === 0) {
-      return Response.json(
+      return NextResponse.json(
         { message: 'User not found', success: false },
         { status: 404 }
       );
     }
 
-    return Response.json(
+    return NextResponse.json(
       { messages: user[0].messages },
-      {
-        status: 200,
-      }
+      { status: 200 }
     );
   } catch (error) {
     console.error('An unexpected error occurred:', error);
-    return Response.json(
+    return NextResponse.json(
       { message: 'Internal server error', success: false },
       { status: 500 }
     );
