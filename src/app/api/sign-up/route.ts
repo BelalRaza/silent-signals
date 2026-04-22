ef41a7ea1bdfe786ce9a0fc4a897c2c776f928cf
Belal Raza <br49009@gmail.com>
2026-04-22 21:57:33 +0530

    Fixed username update bug on signup and ignored .env

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -34,8 +34,8 @@ export async function POST(request:Request) {
                     const hasedPassword = await bcrypt.hash(password, 10)
                     existingUserByEmail.password = hasedPassword;
                     existingUserByEmail.verifyCode = verifyCode;
-                    existingUserByEmail.verifyCodeExpiry = new
-                    Date(Date.now() + 3600000)
+                    existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+                    existingUserByEmail.username = username; // Update the username!
                     await existingUserByEmail.save()
                 }                    
                 
